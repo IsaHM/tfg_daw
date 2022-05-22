181193765a92bdc6cf4ba0e08b8213f23f85e0b8
Isabel HM <isabelhmoyo@gmail.com>
2022-05-22 12:14:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# tfg_daw
+
+Isabel Heredia Moyo
+Desarrollo de Aplicaciones Web, IES El Lago
+Curso 2021/2022
+
+Definición:
+Desarrollo de una página web de iniciación al diseño gráfico enfocada en diseño UI/UX.
---
+++ codigo/landpage.php
@@ -0,0 +1,19 @@
+<?php echo " prueba" ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css\bootstrap.css">
+    <link rel="stylesheet" href="css\custom.css">
+
+    <title>sin_nombre</title>
+</head>
+    <body>
+
+    </body>
+</html>
