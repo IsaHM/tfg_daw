e816925a4b28ef7959d6b8d27bded06ac52eb4e1
Isabel HM <isabelhmoyo@gmail.com>
2022-05-28 22:37:52 +0200

    landpage frontend

--- codigo/landpage.php
+++ codigo/landpage.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS -->
-    <link rel="stylesheet" href="css\bootstrap.css">
-    <link rel="stylesheet" href="css\custom.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/custom.css"> 
+    <link rel="stylesheet" href="css/fuentes.css">
 
-    <title>sin_nombre</title>
+    <!-- JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/portada.js"></script>
+
+    <link rel="icon" type="image/x-icon" href="/imgs/favicon.png">
+    <title>Cursor Academy</title>
 </head>
-    <body>
 
-    </body>
+<body>
+    <header>
+        <div class="menu">
+            <img src="/imgs/500x500_sin_fondo.png" alt="Logotipo Cursor Academy">
+            <ul>
+                <li><a href="#">el proyecto</a></li>
+                <li><a href="#">contacto</a></li>
+                <li><a href="#">productos</a></li>
+                <li><a href="#">prueba de conocimiento</a></li>
+                <li><a href="#">panel administrativo</a></li>
+                <li><a href="#">login</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="portada">
+        <h1 class="texto_portada"><span>cursor</span> academy</h1>
+        <h2>una plataforma para
+            <span
+                class="txt-rotate"
+                data-period="2000"
+                data-rotate='["diseñadores.","programadores.","todo el mundo."]'></span>
+        </h2>
+    </div>
+
+
+</body>
 </html>
